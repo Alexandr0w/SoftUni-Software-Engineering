b311850963813aa77841e43abe696257caae9455
Alexander Alexandrov <alexandrow2505@gmail.com>
2024-06-23 02:55:59 +0300

    Added ExamPreparation

---
+++ 02.CSharp Fundamentals/ExamPreparation/02.MidExamPreparationTwo/03.Numbers/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _03.Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            List<int> greaterThanAverageNumbers = new List<int>();
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+
+            double average = sum / (double)numbers.Length;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] > average)
+                {
+                    greaterThanAverageNumbers.Add(numbers[i]);
+                }
+            }
+            greaterThanAverageNumbers.Sort();
+            greaterThanAverageNumbers.Reverse();
+
+            if (greaterThanAverageNumbers.Count == 0)
+            {
+                Console.WriteLine("No");
+            }
+            else
+            {
+                int count = greaterThanAverageNumbers.Count > 5 ? 5 : greaterThanAverageNumbers.Count;
+                Console.WriteLine(string.Join(" ", greaterThanAverageNumbers.Take(count)));
+            }
+        }
+    }
+}
