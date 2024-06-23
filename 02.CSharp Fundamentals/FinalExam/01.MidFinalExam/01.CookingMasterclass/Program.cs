a4f5df5e49c1e4de976a066928d4c27707ba0858
Alexander Alexandrov <alexandrow2505@gmail.com>
2024-06-23 12:13:18 +0300

    Added MidFinalExam

---
+++ 02.CSharp Fundamentals/FinalExam/01.MidFinalExam/01.CookingMasterclass/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _01.CookingMasterclass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int students = int.Parse(Console.ReadLine());
+            double PackageOfFlourPrice = double.Parse(Console.ReadLine());
+            double SingleEggPrice = double.Parse(Console.ReadLine());
+            double SingleApronPrice = double.Parse(Console.ReadLine());
+
+            int freePackages = students / 5;
+
+            int apronsNeeded = (int)Math.Ceiling(students * 1.2);
+
+            double totalSum = (SingleApronPrice * apronsNeeded) +
+                              (SingleEggPrice * 10 * students) +
+                              (PackageOfFlourPrice * (students - freePackages));
+            if (budget >= totalSum)
+            {
+                Console.WriteLine($"Items purchased for {totalSum:F2}$.");
+            }
+            else
+            {
+                double neededMoney = Math.Abs(totalSum - budget);
+                Console.WriteLine($"{neededMoney:F2}$ more needed.");
+            }
+        }
+    }
+}
