05ce4348af4cb2b8561d5c8761bd11e8655a5f28
Alexander Alexandrov <alexandrow2505@gmail.com>
2024-06-23 21:21:48 +0300

    Added Exam Preparation

---
+++ 02.CSharp Fundamentals/ExamPreparation/01.MidExamPreparationOne/01.Counter-Strike/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _01.Counter_Strike
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int energy = int.Parse(Console.ReadLine());
+            int wins = 0;
+
+            string command;
+            while ((command = Console.ReadLine()) != "End of battle")
+            {
+                int distance = int.Parse(command);
+
+                if (energy < distance)
+                {
+                    Console.WriteLine($"Not enough energy! Game ends with {wins} won battles and {energy} energy");
+                    break;
+                }
+
+                wins++;
+                energy -= distance;
+
+                if (wins % 3 == 0)
+                {
+                    energy += wins;
+                }
+            }
+
+            if (command == "End of battle")
+            {
+                Console.WriteLine($"Won battles: {wins}. Energy left: {energy}");
+            }
+        }
+    }
+}
