390307f85a14833c6ba0ccca14a76ac568c63ce9
Alexander Alexandrov <alexandrow2505@gmail.com>
2024-06-26 11:51:15 +0300

    Updated

--- 01.CSharp Programming Basics/FinalExam/03.ExcursionCalculator/Program.cs
+++ 01.CSharp Programming Basics/FinalExam/03.ExcursionCalculator/Program.cs
@@ -1,57 +1,52 @@
 ﻿int countOfPeople = int.Parse(Console.ReadLine());
-string season  = Console.ReadLine();
+string season = Console.ReadLine();
 double priceForNight = 0;
 
 switch (season)
 {
     case "spring":
+        if (countOfPeople > 5)
         {
-            if (countOfPeople > 5)
-            {
-                priceForNight = 48;
-            }
-            else
-            {
-                priceForNight = 50;
-            }
-            break;
+            priceForNight = 48;
         }
+        else
+        {
+            priceForNight = 50;
+        }
+        break;
+
     case "summer":
+        if (countOfPeople > 5)
         {
-            if (countOfPeople > 5)
-            {
-                priceForNight = 45;
-            }
-            else
-            {
-                priceForNight = 48.50;
-            }
-            break;
+            priceForNight = 45;
         }
+        else
+        {
+            priceForNight = 48.50;
+        }
+        break;
+
     case "autumn":
+        if (countOfPeople > 5)
         {
-            if (countOfPeople > 5)
-            {
-                priceForNight = 49.50;
-            }
-            else
-            {
-                priceForNight = 60.00;
-            }
-            break;
+            priceForNight = 49.50;
         }
+        else
+        {
+            priceForNight = 60.00;
+        }
+        break;
+
     case "winter":
+        if (countOfPeople > 5)
         {
-            if (countOfPeople > 5)
-            {
-                priceForNight = 85.00;
-            }
-            else
-            {
-                priceForNight = 86.00;
-            }
-            break;
+            priceForNight = 85.00;
         }
+        else
+        {
+            priceForNight = 86.00;
+        }
+        break;
 }
 double totalPrice = priceForNight * countOfPeople;
 if (season == "summer")
