e7f8110a370d2194c065e837f88518a1a663a735
Alexander Alexandrov <alexandrow2505@gmail.com>
2024-06-27 20:59:41 +0300

    Added Objects And Classes

---
+++ 02.CSharp Fundamentals/06.ObjectsAndClasses-Lab/04.Students/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _04.Students
+{
+    internal class Program
+    {
+        class Student
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public int Age { get; set; }
+            public string City { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            string[] information = Console.ReadLine().Split(' ');
+            List<Student> students = new List<Student>();
+
+            while (information[0] != "end")
+            {
+                Student student = new Student();
+                student.FirstName = information[0];
+                student.LastName = information[1];
+                student.Age = (int.Parse(information[2]));
+                student.City = information[3];
+
+                students.Add(student);
+
+                information = Console.ReadLine().Split(' ');
+            }
+
+            string filterCity = Console.ReadLine();
+
+            CheckEveryStudent(students, filterCity);
+        }
+
+        static void CheckEveryStudent(List<Student> students, string? filterCity)
+        {
+            foreach (Student student in students)
+            {
+                if (student.City == filterCity)
+                {
+                    Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+                }
+            }
+        }
+    }
+}
---
+++ 02.CSharp Fundamentals/06.ObjectsAndClasses-Lab/06.StoreBoxes/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace _06.StoreBoxes
+{
+    internal class Program
+    {
+        class Item
+        {
+            public Item(string name, double price)
+            {
+                this.Name = name;
+                this.Price = price;
+            }
+            public string Name { get; set; }
+            public double Price { get; set; }
+        }
+
+        class Box
+        {
+            public string SerialNumber { get; set; }
+            public Item Item { get; set; }
+            public int ItemQuantity { get; set; }
+            public double PriceOfOneBox
+            {
+                get
+                {
+                    return this.ItemQuantity * this.Item.Price;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            string[] command = Console.ReadLine().Split(' ');
+            List<Box> boxes = new List<Box>();
+
+            while (command[0] != "end")
+            {
+                Box box = new Box
+                {
+                    SerialNumber = command[0],
+                    ItemQuantity = int.Parse(command[2]),
+                    Item = new Item(command[1], double.Parse(command[3]))
+                };
+
+                boxes.Add(box);
+                command = Console.ReadLine().Split(' ');
+            }
+
+            List<Box> orderBoxes = boxes.OrderByDescending(box => box.PriceOfOneBox).ToList();
+
+            foreach (Box box in orderBoxes)
+            {
+                Console.WriteLine(box.SerialNumber);
+                Console.WriteLine($"-- {box.Item.Name} - ${box.Item.Price:f2}: {box.ItemQuantity}");
+                Console.WriteLine($"-- ${box.PriceOfOneBox:f2}");
+            }
+        }
+    }
+}
