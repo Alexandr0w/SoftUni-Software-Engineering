1fa5e2dd643213ce0d00cf90f7383824292a94c4
Alexander Alexandrov <alexandrow2505@gmail.com>
2024-07-10 14:29:58 +0300

    Added Associative Arrays

---
+++ 02.CSharp Fundamentals/07.AssociativeArrays-Exercise/06.StudentAcademy/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _06.StudentAcademy
+{
+    internal class Program
+    {
+        class Student
+        {
+            public Student(string name)
+            {
+                Grades = new List<double>();
+                Name = name;
+            }
+
+            public string Name { get; set; }
+
+            public List<double> Grades { get; set; }
+
+            public override string ToString()
+            {
+
+                return $"{Name} -> {Grades.Average():F2}";
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Dictionary<string, Student> students = new Dictionary<string, Student>();
+           
+            int studentsCount = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= studentsCount; i++)
+            {
+                string studentName = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+
+                if (!students.ContainsKey(studentName))
+                {
+                    students.Add(studentName, new Student(studentName));
+                }
+
+                students[studentName].Grades.Add(grade);
+            }
+
+            var filteredStudents = students.Where(g => g.Value.Grades.Average() >= 4.50);
+
+            foreach (var pair in filteredStudents)
+            {
+                Console.WriteLine(pair.Value.ToString());
+            }
+        }
+    }
+}
---
+++ 02.CSharp Fundamentals/07.AssociativeArrays-Lab/01.CountRealNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _01.CountRealNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List <int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            SortedDictionary<int, int> count = new SortedDictionary<int, int>();
+
+            foreach (var number in numbers)
+            {
+                if (!count.ContainsKey(number))
+                {
+                    count.Add(number, 1);
+                }
+                else
+                {
+                    count[number]++;
+                }
+            }
+
+            foreach (var number in count)
+            {
+                Console.WriteLine($"{number.Key} -> {number.Value}");
+            }
+        }
+    }
+}
