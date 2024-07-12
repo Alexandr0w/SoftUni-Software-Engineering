adc77a1985c9a9d91a5853eeddb83ef1ba4d2796
Alexander Alexandrov <alexandrow2505@gmail.com>
2024-07-12 16:24:43 +0300

    Added Text Processing

---
+++ 02.CSharp Fundamentals/08.TextProcessing-Lab/05.Digits,LettersAndOther/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _05.Digits_LettersAndOther
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            char[] digits = input.Where(chr => char.IsDigit(chr)).ToArray();
+            char[] letters = input.Where(chr => char.IsLetter(chr)).ToArray();
+            char[] others = input.Where(chr => !char.IsLetterOrDigit(chr)).ToArray();
+
+            Console.WriteLine(digits);
+            Console.WriteLine(letters);
+            Console.WriteLine(others);
+        }
+    }
+}
