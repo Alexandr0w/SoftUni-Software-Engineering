328cbe54694fb95c4527410d386dcdace51c458f
Alexander Alexandrov <alexandrow2505@gmail.com>
2024-07-16 10:09:07 +0300

    Added Text Processing

---
+++ 02.CSharp Fundamentals/08.TextProcessing-Exercise/01.Valid Usernames/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _01.Valid_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] usernames = Console.ReadLine().Split(", ");
+
+            foreach (string username in usernames)
+            {
+                if (username.Length < 3 || username.Length > 16)
+                {
+                    continue;
+                }
+
+                bool isValidName = username.All(character => char.IsLetterOrDigit(character) || 
+                                                             character == '-' || 
+                                                             character == '_');
+                
+                if (isValidName)
+                {
+                    Console.WriteLine(username);
+                }
+            }
+        }
+    }
+}
---
+++ 02.CSharp Fundamentals/08.TextProcessing-Exercise/06.ReplaceRepeatingChars/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace _06.ReplaceRepeatingChars
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            string result = string.Empty;
+
+            if (!string.IsNullOrEmpty(input))
+            {
+                result = input[0].ToString();
+
+                for (int i = 1; i < input.Length; i++)
+                {
+                    if (input[i] != input[i - 1])
+                    {
+                        result += input[i];
+                    }
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            input.Any(s => s == '+');
+            Console.WriteLine(result);
+        }
+    }
+}
