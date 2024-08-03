7bd1646a2696d33da14d1d3ab444cb56ed16aec3
Alexander Alexandrov <alexandrow2505@gmail.com>
2024-08-04 01:57:13 +0300

    Ädded Final Exam Preparation

---
+++ 02.CSharp Fundamentals/ExamPreparation/01.FinalExamPreparationOne/02.DestinationMapper/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02.DestinationMapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            MatchCollection matches = Regex.Matches(input, @"([=\/])([A-Z][A-Za-z]{2,})\1");
+
+            List<string> results = new List<string>();
+
+            int charSum = 0;
+
+            foreach (Match match in matches)
+            {
+                results.Add(match.Groups[2].Value);
+                charSum += match.Groups[2].Value.Length;
+            }
+
+            Console.WriteLine($"Destinations: {String.Join(", ", results)}");
+            Console.WriteLine($"Travel Points: {charSum}");
+        }
+    }
+}
---
+++ 02.CSharp Fundamentals/ExamPreparation/02.FinalExamPreparationTwo/02.MirrorWords/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02.MirrorWords
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+
+            string pattern = @"(@|#)(?<wordOne>[A-Za-z]{3,})\1\1(?<wordTwo>[A-Za-z]{3,})\1";
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matches = regex.Matches(input);
+
+            List<string> validPairs = new List<string>();
+            List<string> mirrorWords = new List<string>();
+
+            foreach (Match match in matches)
+            {
+                string wordOne = match.Groups["wordOne"].Value;
+                string wordTwo = match.Groups["wordTwo"].Value;
+                validPairs.Add($"{wordOne} <=> {wordTwo}");
+
+                string reversedWordTwo = new string(wordTwo.Reverse().ToArray());
+                if (wordOne == reversedWordTwo)
+                {
+                    mirrorWords.Add($"{wordOne} <=> {wordTwo}");
+                }
+            }
+
+            if (validPairs.Count == 0)
+            {
+                Console.WriteLine("No word pairs found!");
+            }
+            else
+            {
+                Console.WriteLine($"{validPairs.Count} word pairs found!");
+            }
+
+            if (mirrorWords.Count == 0)
+            {
+                Console.WriteLine("No mirror words!");
+            }
+            else
+            {
+                Console.WriteLine("The mirror words are:");
+                Console.WriteLine(string.Join(", ", mirrorWords));
+            }
+        }
+    }
+}
