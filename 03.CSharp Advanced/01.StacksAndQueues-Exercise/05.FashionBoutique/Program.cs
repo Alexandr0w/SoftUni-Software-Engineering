b4692e4d67b0f98415f2ec2694218707963dd1d8
Alexander Alexandrov <alexandrow2505@gmail.com>
2024-09-23 23:37:50 +0300

    Added Stack and Queues

---
+++ 03.CSharp Advanced/01.StacksAndQueues-Exercise/05.FashionBoutique/Program.cs
@@ -0,0 +1,22 @@
+﻿Stack<int> clothes = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+
+int capacity = int.Parse(Console.ReadLine());
+int racks = 1;
+int currentCapacity = capacity;
+
+while (clothes.Count > 0)
+{
+    int currentClothes = clothes.Pop();
+
+    if (currentClothes <= currentCapacity)
+    {
+        currentCapacity -= currentClothes;
+    }
+    else
+    {
+        racks++;
+        currentCapacity = capacity - currentClothes;
+    }
+}
+
+Console.WriteLine(racks);
---
+++ 03.CSharp Advanced/01.StacksAndQueues-Lab/6. Supermarket/Program.cs
@@ -0,0 +1,22 @@
+﻿string input = Console.ReadLine();
+
+Queue<string> customer = new Queue<string>();
+
+while (input != "End")
+{
+    if (input == "Paid")
+    {
+        while (customer.Count > 0)
+        {
+            Console.WriteLine(customer.Dequeue());
+        }
+    }
+    else
+    {
+        customer.Enqueue(input);
+    }
+
+    input = Console.ReadLine();
+}
+
+Console.WriteLine(customer.Count + " people remaining.");
