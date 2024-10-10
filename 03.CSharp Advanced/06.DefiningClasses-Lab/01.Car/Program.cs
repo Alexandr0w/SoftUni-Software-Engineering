61d76a86b9fb1a421c6afb47e281d5672b505beb
Alexandr0w <alexandrow2505@gmail.com>
2024-10-10 17:09:27 +0300

    AD

---
+++ 03.CSharp Advanced/06.DefiningClasses-Lab/01.Car/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Car car = new Car()
+            {
+                Make = "BMW",
+                Model = "5",
+                Year = 2008
+            };
+
+            Console.WriteLine($"{car.Make} - {car.Model} - {car.Year}");
+        }
+    }
+}
---
+++ 03.CSharp Advanced/06.DefiningClasses-Lab/04.CarEngineAndTires/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Car defaultCar = new Car();
+            Console.WriteLine(defaultCar.WhoAmI());
+
+            Car car = new Car("BMW", "3", 1997);
+            Console.WriteLine(car.WhoAmI());
+
+            Car fullCar = new Car("BMW", "3", 1997, 60, 25);
+            Console.WriteLine(fullCar.WhoAmI());
+
+            Tire[] tires = new Tire[2]
+            {
+                new Tire(2000, 2),
+                new Tire(2000, 3)
+            };
+
+            Engine engine = new Engine(225, 3000);
+
+            Car enginedCar = new Car("BMW", "3", 1997, 60, 25, engine, tires);
+        }
+    }
+}
