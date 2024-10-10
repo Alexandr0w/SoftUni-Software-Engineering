fccd448a8a4485152c0af7a6b981e49bb3560ea5
Alexander Alexandrov <alexandrow2505@gmail.com>
2024-10-10 17:51:41 +0300

    Added exercises from SetsAndDictionariesAdvanced-Lab

---
+++ 03.CSharp Advanced/03.SetsAndDictionariesAdvanced-Lab/03.Larger3Numbers/Program.cs
@@ -0,0 +1,11 @@
+﻿List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+<<<<<<< HEAD
+numbers = numbers.OrderByDescending(number => number)
+=======
+numbers = numbers.OrderByDescending(number => number) //
+>>>>>>> 48d7ae599d31b28b2513dc57f8172fcb65822ad4
+    .Take(3)
+    .ToList();
+
+Console.WriteLine($"{string.Join(" ", numbers)}");
