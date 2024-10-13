e71b287b06dea14b2cd66a12ebd1b6b694cb47e8
Alexander Alexandrov <alexandrow2505@gmail.com>
2024-10-13 03:49:37 +0300

    Added exercises from Streams,FilesAndDirectories-Lab

---
+++ 03.CSharp Advanced/04.Streams,FilesAndDirectories-Lab/03.WordCount/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace WordCount
+{
+    public class WordCount
+    {
+        static void Main()
+        {
+            string wordPath = @"..\..\..\Files\words.txt";
+            string textPath = @"..\..\..\Files\text.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            CalculateWordCounts(wordPath, textPath, outputPath);
+        }
+
+        public static void CalculateWordCounts(string wordsFilePath, string textFilePath, string outputFilePath)
+        {
+            Dictionary<string, int> wordCounts = new Dictionary<string, int>();
+
+            using (StreamReader wordReader = new StreamReader(wordsFilePath))
+            {
+                string[] words = wordReader.ReadLine().Split();
+
+                for (int i = 0; i < words.Length; i++)
+                {
+                    string word = words[i];
+
+                    if (!wordCounts.ContainsKey(word))
+                    {
+                        wordCounts.Add(word, 0);
+                    }
+                }
+            }
+
+            using (StreamReader textReader = new StreamReader(textFilePath))
+            {
+                string line = textReader.ReadLine();
+
+                while (line != null)
+                {
+                    line = line.ToLower();
+
+                    foreach (var word in wordCounts)
+                    {
+                        if (line.Contains(word.Key))
+                        {
+                            wordCounts[word.Key]++;
+                        }
+                    }
+
+                    line = textReader.ReadLine();
+                }
+            }
+
+            using (StreamReader writer = new StreamReader(outputFilePath))
+            {
+                wordCounts = wordCounts.OrderByDescending(w => w.Value).ToDictionary(w => w.Key, w => w.Value);
+
+                foreach (var word in wordCounts)
+                {
+                    Console.WriteLine($"{word.Key} - {word.Value}");
+                }
+            }
+        }
+    }
+}
---
+++ 03.CSharp Advanced/04.Streams,FilesAndDirectories-Lab/07.FolderSize/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace FolderSize
+{
+    public class FolderSize
+    {
+        static void Main()
+        {
+            string folderPath = @"..\..\..\Files\TestFolder";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            GetFolderSize(folderPath, outputPath);
+        }
+
+        public static void GetFolderSize(string folderPath, string outputFilePath)
+        {
+            double sum = 0;
+
+            DirectoryInfo dir = new DirectoryInfo(folderPath);
+            FileInfo[] infos = dir.GetFiles("*", SearchOption.AllDirectories);
+
+            foreach (var fileInfo in infos)
+            {
+                sum += fileInfo.Length;
+            }
+
+            File.WriteAllText(outputFilePath, $"{sum / 2048} KB");
+        }
+    }
+}
