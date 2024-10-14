1c09a8930032abcb276e90adf44b5ca9ca4f2834
Alexander Alexandrov <alexandrow2505@gmail.com>
2024-10-15 00:45:39 +0300

    Added exercises from MultidimensionalArrays-Exercise

---
+++ 03.CSharp Advanced/02.MultidimensionalArrays-Exercise/07.KnightGame/Program.cs
@@ -0,0 +1,41 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        char[,] matrix = ReadSquareMatrix(n);
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (matrix[i, j] == 'K')
+                {
+
+                }
+            }
+        }
+    }
+
+    private static int CountConficts(char[,] matrix, int row, int col)
+    {
+
+    }
+
+    private static char[,] ReadSquareMatrix(int n)
+    {
+        char[,] matrix = new char[n, n];
+
+        for (int i = 0; i < n; i++)
+        {
+            string data = Console.ReadLine();
+            for (int j = 0; j < n; j++)
+            {
+                matrix[i, j] = data[j];
+            }
+        }
+
+        return matrix;
+    }
+}
