7ef609bd885118f324d906ced637d5118ac086c5
Alexandr0w <alexandrow2505@gmail.com>
2024-10-16 19:52:22 +0300

    Added exercises from FunctionalProgramming-Exercise

---
+++ 03.CSharp Advanced/05.FunctionalProgramming-Exercise/04.FindEvensOrOdds/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+int lowerNumber = numbers[0];
+int upperNumber = numbers[1];
+
+string parity = Console.ReadLine();
+
+Predicate<int> isValid = x => x % 2 == 0;
+
+for (int i = lowerNumber; i < upperNumber; i++)
+{
+    if (parity == "even")
+    else if (parity == "odd")
+}    
