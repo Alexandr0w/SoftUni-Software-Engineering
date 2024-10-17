c5960cfc27334fe09a09b3836749aa20e19b8eb6
Alexandr0w <alexandrow2505@gmail.com>
2024-10-17 17:30:08 +0300

    Added exercises from Generics-Lab

---
+++ 03.CSharp Advanced/08.Generics-Lab/03.GenericScale/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace GenericScale
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            EqualityScale<int> equalityScale = new EqualityScale<int>(7, 3);
+
+            Console.WriteLine(equalityScale.IsLeftBigger());
+
+        }
+    }
+}
