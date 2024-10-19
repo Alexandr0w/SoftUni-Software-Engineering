7d855a4665d3a918b50e40a6376ab18d055bbfe6
Alexander Alexandrov <alexandrow2505@gmail.com>
2024-10-19 20:51:27 +0300

    Added exercises from IteratorsAndComparators-Exercise

---
+++ 03.CSharp Advanced/09.IteratorsAndComparators-Exercise/01.ListyIterator/ListyIterator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private readonly List<T> _list;
+        private int _index;
+
+        public ListyIterator(List<T> list) 
+        {
+            this._list = list ?? throw new ArgumentException(nameof(list));
+        }
+
+        public bool Move()
+        {
+            if (!this.HasNext()) return false;
+
+            this._index++;
+            return true;
+        }
+
+        public bool HasNext() => this._index + 1 < this._list.Count;
+
+        public void Print()
+        {
+            if (this._index >= this._list.Count) throw new InvalidOperationException("Invalid Operation!");
+
+            Console.WriteLine(this._list[this._index]);
+        }
+    }
+}
---
+++ 03.CSharp Advanced/09.IteratorsAndComparators-Exercise/03.Stack/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Stack
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+
+            string command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                if (command.StartsWith("Push"))
+                {
+                    IEnumerable<int> data = command.Substring(5).Split(", ").Select(int.Parse);
+
+                    foreach (int number in data)
+                    {
+                        stack.Push(number);
+                    }
+                }
+                else if (command == "Pop")
+                {
+                    if (stack.Count == 0) Console.WriteLine("No elements");
+                    else stack.Pop();
+                }
+
+                command = Console.ReadLine();
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                foreach (int number in stack)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+        }
+    }
+}   
