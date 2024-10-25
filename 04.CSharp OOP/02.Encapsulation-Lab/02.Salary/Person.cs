6988c98d400ba4c10a867e1b854875c61073e437
Alexandr0w <alexandrow2505@gmail.com>
2024-10-25 15:18:16 +0300

    Added exercises from Encapsulation-Lab

---
+++ 04.CSharp OOP/02.Encapsulation-Lab/02.Salary/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int Age { get; private set; }
+        public decimal Salary { get; private set; }
+
+        public override string ToString() => $"{FirstName} {LastName} receives {Salary:F2} leva.";
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            decimal salaryIncrease = Salary * percentage / 100;
+            if (Age < 30)
+            {
+                salaryIncrease /= 2;
+            }
+
+            Salary += salaryIncrease;
+        }
+    }
+}
---
+++ 04.CSharp OOP/02.Encapsulation-Lab/04.Team/Team.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+    }
+}
