0a48f9e49ebc3272e17dbe7848924029bcaeb092
Alexander Alexandrov <alexandrow2505@gmail.com>
2024-10-25 17:26:19 +0300

    Added exercises from ExamPreparation

--- 03.CSharp Advanced/ExamPreparation/03.RegularExam-17February2024/03.SharkTaxonomy/Classifier.cs
+++ 03.CSharp Advanced/ExamPreparation/03.RegularExam-17February2024/03.SharkTaxonomy/Classifier.cs
@@ -1,56 +1,56 @@
-﻿using System.Security.Cryptography;
-using System.Text;
+﻿using System.Text;
 
-namespace SharkTaxonomy
+namespace SharkTaxonomy;
+
+public class Classifier
 {
-    public class Classifier
+    public Classifier(int capacity)
     {
-        public Classifier(int capacity) 
+        this.Capacity = capacity;
+        this.Species = new List<Shark>();
+    }
+
+    public int Capacity { get; }
+    public List<Shark> Species { get; }
+    public int GetCount => this.Species.Count;
+
+    public void AddShark(Shark shark)
+    {
+        if (this.Species.Count >= this.Capacity || this.Species.Any(s => s.Kind == shark.Kind)) return;
+
+        this.Species.Add(shark);
+    }
+
+    public bool RemoveShark(string kind)
+    {
+        Shark? sharkToRemove = this.Species.FirstOrDefault(s => s.Kind == kind);
+        if (sharkToRemove is null) return false;
+
+        return this.Species.Remove(sharkToRemove);
+    }
+
+    public string GetLargestShark()
+    {
+        return this.Species.MaxBy(x => x.Length)!.ToString();
+    }
+
+    public double GetAverageLength()
+    {
+        return this.Species.Average(x => x.Length);
+    }
+
+    public string Report()
+    {
+        StringBuilder sb = new StringBuilder();
+
+        sb.Append($"{this.GetCount} sharks classified:");
+
+        foreach (Shark shark in this.Species)
         {
-            this.Capacity = capacity;
-            this.Species = new List<Shark>();
-        }
-        public int Capacity { get; }
-        public List<Shark> Species { get; }
-
-        public int GetCount => this.Species.Count;
-
-        public void AddShark(Shark shark)
-        {
-            if (this.Species.Count >= this.Capacity || this.Species.Any(s => s.Kind == shark.Kind)) return;
-            
-            this.Species.Add(shark);
+            sb.AppendLine();
+            sb.Append(shark.ToString());
         }
 
-        public bool RemoveShark(string kind)
-        {
-            Shark? sharkToRemove = this.Species.SingleOrDefault(s => s.Kind == kind);
-            if (sharkToRemove == null) return false;
-             
-            return this.Species.Remove(sharkToRemove);
-        }
-
-        public string GetLargestShark()
-        {
-            return this.Species.MaxBy(x => x.Length)!.ToString();
-        }
-
-        public double GetAverageLength()
-        {
-            return this.Species.Average(x => x.Length);
-        }
-
-        public string Report()
-        {
-            StringBuilder report = new StringBuilder();
-            report.Append($"{this.GetCount} sharks classifield:");
-
-            foreach (Shark shark in this.Species)
-            {
-                report.AppendLine($"{shark}");
-            }
-
-            return report.ToString().Trim();
-        }
+        return sb.ToString();
     }
 }
