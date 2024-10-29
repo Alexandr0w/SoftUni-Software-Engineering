8c94bd4f6dccd023263caf3ecae522b37acb1578
Alexander Alexandrov <alexandrow2505@gmail.com>
2024-10-30 01:10:22 +0200

    Added exercises from InterfacesАndAbstraction-Lab

---
+++ 04.CSharp OOP/03.InterfacesАndAbstraction-Lab/02.Cars/BaseCar.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public abstract class BaseCar : ICar
+    {
+        protected BaseCar(string model, string color)
+        {
+            this.Model = model;
+            this.Color = color;
+        }
+
+        public string Model { get; }
+
+        public string Color { get; }
+
+        public string Start() => "Engine start";
+
+        public string Stop() => "Breaaak!";
+
+        protected virtual string Describe() => $"{this.Color} {this.GetType().Name} {this.Model}";
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(this.Describe());
+            sb.AppendLine(this.Start());
+            sb.Append(this.Stop());
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ 04.CSharp OOP/03.InterfacesАndAbstraction-Lab/02.Cars/Tesla.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class Tesla : BaseCar, ICar, IElectricCar
+    {
+        public Tesla(string model, string color, int battery) : base(model, color)
+        {
+            this.Battery = battery;
+        }
+
+        public int Battery { get; }
+
+        protected override string Describe() => $"{base.Describe()} with {this.Battery} Batteries";
+    }
+}
