5a865fd71d5dcc998247156d04db926bc3a1bc4d
Alexandr0w <alexandrow2505@gmail.com>
2024-10-30 17:04:58 +0200

    Added exercises from InterfacesAndAbstraction-Exercise

---
+++ 04.CSharp OOP/03.InterfacesAndAbstraction-Exercise/03.Telephony/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Telephony
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            StationaryPhone stationaryPhone = new StationaryPhone();
+            SmartPhone smartPhone = new SmartPhone();
+
+            string[] phoneNumbers = Console.ReadLine()!.Split();
+            string[] urls = Console.ReadLine()!.Split();
+
+            Call(phoneNumbers, pn =>
+            {
+                if (pn.Length == 7) return stationaryPhone;
+                if (pn.Length == 10) return smartPhone;
+                return null;
+            });
+            Browse(urls, smartPhone);
+
+        }
+
+        private static void Call(string[] phoneNumbers, Func<string, ICaller?> provideCaller)
+        {
+            foreach (string phoneNumber in phoneNumbers)
+            {
+                if (!Call(phoneNumber, provideCaller))
+                {
+                    Console.WriteLine("Invalid number!");
+                }
+            }
+        }
+
+        private static bool Call(string phoneNumber, Func<string, ICaller?> provideCaller)
+        {
+            if (phoneNumber.Any(s => !char.IsDigit(s))) return false;
+            
+            ICaller? caller = provideCaller(phoneNumber);
+            if (caller is null) return false;
+            Console.WriteLine(caller.Call(phoneNumber));
+                
+            return true;
+        }
+
+        private static void Browse(string[] urls, IBrowser browser)
+        {
+            foreach (string url in urls)
+            {
+                if (!Browse(url, browser))
+                {
+                    Console.WriteLine("Invalid URL!");
+                }
+            }
+        }
+
+        private static bool Browse(string url, IBrowser browser)
+        {
+            if (url.Any(char.IsDigit)) return false;
+
+            Console.WriteLine(browser.Browse(url));
+
+            return true;
+        }
+    }
+}
---
+++ 04.CSharp OOP/03.InterfacesAndAbstraction-Exercise/03.Telephony/SmartPhone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telephony
+{
+    public class SmartPhone : IBrowser, ICaller
+    {
+        public string Browse(string url) => $"Browsing: {url}!";
+
+        public string Call(string phoneNumber) => $"Calling... {phoneNumber}";
+    }
+}
