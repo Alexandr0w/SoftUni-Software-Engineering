c63830345c040bd0daef1eddee81f1ba6c7daaea
Alexandr0w <alexandrow2505@gmail.com>
2024-11-19 23:22:01 +0200

    Added exercises from ReflectionAndAttributes-Lab

---
+++ 04.CSharp OOP/07.ReflectionAndAttributes-Lab/06.CodeTracker/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using AuthorProblem;
+using AuthorProblem.Attributes;
+using System.Reflection;
+
+namespace CodeTracker
+{
+    [Author("Victor")]
+    public class StartUp
+    {
+        [Author("George")]
+        public static void Main()
+        {
+            var tracker = new Tracker();
+            tracker.PrintMethodsByAuthor(Assembly.GetExecutingAssembly());
+        }
+    }
+
+}
---
+++ 04.CSharp OOP/07.ReflectionAndAttributes-Lab/06.CodeTracker/Tracker.cs
@@ -0,0 +1,22 @@
+﻿using AuthorProblem.Attributes;
+using System.Reflection;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor(Assembly assembly)
+        {
+            foreach (Type type in assembly.GetTypes())
+            {
+                foreach (MethodInfo method in type.GetMethods())
+                {
+                    foreach (AuthorAttribute attribute in method.GetCustomAttributes<AuthorAttribute>())
+                    {
+                        Console.WriteLine($"{method.Name} is written by {attribute.Name}");
+                    }
+                }
+            }
+        }
+    }
+}
