3b971ca4be0b36615a1c904d8c71456104e44d2b
Alexandr0w <alexandrow2505@gmail.com>
2024-11-20 16:12:44 +0200

    Added exercises from ReflectionAndAttributes-Exercise

---
+++ 04.CSharp OOP/07.ReflectionAndAttributes-Exercise/01.CommandPattern/Core/Commands/ExitCommand.cs
@@ -0,0 +1,13 @@
+﻿using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Core.Commands
+{
+    public class ExitCommand : ICommand
+    {
+        public string Execute(string[] args)
+        {
+            Environment.Exit(exitCode: 0);
+            return string.Empty;
+        }
+    }
+}
---
+++ 04.CSharp OOP/07.ReflectionAndAttributes-Exercise/02.ValidationAttributes/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace ValidationAttributes
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Person person = new Person("Michael", 12);
+
+            bool isValidEntity = Validator.IsValid(person);
+
+            Console.WriteLine(isValidEntity);
+        }
+    }
+}
