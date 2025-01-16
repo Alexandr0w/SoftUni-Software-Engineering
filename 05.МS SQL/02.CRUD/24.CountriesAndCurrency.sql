332ed2b82e0eb8210a94ad92d927645a8634265f
Alexandr0w <alexandrow2505@gmail.com>
2025-01-16 11:44:11 +0200

    Added exercises from CRUD

--- 05.МS SQL/02.CRUD/24.CountriesAndCurrency.sql
+++ 05.МS SQL/02.CRUD/24.CountriesAndCurrency.sql
@@ -1,9 +1,9 @@
   SELECT [CountryName]
-        ,[CountryCode]
-	    ,[CurrencyCode] =
-    CASE [CurrencyCode]
-    WHEN 'EUR' THEN 'Euro'
-    ELSE 'Not Euro'
-     END
+        ,[CountryCode],
+        CASE
+			WHEN [CurrencyCode] = 'EUR' THEN 'Euro'
+			ELSE 'Not Euro'
+		END
+	  AS [Currency]
     FROM [Countries]
 ORDER BY [CountryName]
