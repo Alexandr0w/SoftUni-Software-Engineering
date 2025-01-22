983f2456ac8bf70c27c34e21a109fb8ff6363077
Alexandr0w <alexandrow2505@gmail.com>
2025-01-22 17:30:41 +0200

    Added exercises from SubqueriesAndJoins

---
+++ 05.МS SQL/05.SubqueriesAndJoins/01.EmployeeAddress.sql
@@ -0,0 +1,8 @@
+SELECT TOP(5) 
+	e.[EmployeeId], 
+	e.[JobTitle], 
+	a.[AddressId], 
+	a.[AddressText] 
+FROM [Employees] AS e
+JOIN [Addresses] AS a ON e.[AddressID] = a.[AddressID]
+ORDER BY [AddressId]
