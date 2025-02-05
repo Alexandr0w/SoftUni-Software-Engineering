c1888bc03441399d99051b0810e8c702dd40ca90
Alexandr0w <alexandrow2505@gmail.com>
2025-02-05 17:45:48 +0200

    Added exercises from Exam Preparation 1

--- 05.МS SQL/ExamPreparation/01.MSSQLRetakeExam-7August2024/03.Update.sql
+++ 05.МS SQL/ExamPreparation/01.MSSQLRetakeExam-7August2024/03.Update.sql
@@ -1,3 +1,7 @@
 UPDATE [Shoes]
 SET [Price] = [Price] * 1.15
-WHERE [BrandId] IN (SELECT [Id] FROM [Brands] WHERE [Name] = 'Nike')
+WHERE [BrandId] IN (
+	SELECT [Id] 
+	FROM [Brands] 
+	WHERE [Name] = 'Nike'
+)
--- 05.МS SQL/ExamPreparation/01.MSSQLRetakeExam-7August2024/04.Delete.sql
+++ 05.МS SQL/ExamPreparation/01.MSSQLRetakeExam-7August2024/04.Delete.sql
@@ -1,9 +1,18 @@
 DELETE 
 FROM [Orders] 
-WHERE [ShoeId] IN (SELECT Id FROM Shoes WHERE Model = 'Joyride Run Flyknit')
+WHERE [ShoeId] IN (
+	SELECT [Id] 
+	FROM [Shoes] 
+	WHERE [Model] = 'Joyride Run Flyknit'
+)
+
 DELETE 
 FROM [ShoesSizes] 
-WHERE [ShoeId] IN (SELECT Id FROM Shoes WHERE Model = 'Joyride Run Flyknit')
+WHERE [ShoeId] IN (
+SELECT [Id] 
+FROM [Shoes]
+ WHERE [Model] = 'Joyride Run Flyknit')
+
 DELETE 
 FROM [Shoes] 
 WHERE [Model] = 'Joyride Run Flyknit'
