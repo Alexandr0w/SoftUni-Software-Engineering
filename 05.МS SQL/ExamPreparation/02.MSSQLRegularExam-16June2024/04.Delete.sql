0225d8f2a979765f5e67ef178d36aee73a1598a8
Alexandr0w <alexandrow2505@gmail.com>
2025-02-05 17:50:30 +0200

    Added exercises from Exam Preparation 2

--- 05.МS SQL/ExamPreparation/02.MSSQLRegularExam-16June2024/04.Delete.sql
+++ 05.МS SQL/ExamPreparation/02.MSSQLRegularExam-16June2024/04.Delete.sql
@@ -1,11 +1,23 @@
 DELETE 
 FROM [LibrariesBooks]
-WHERE [BookId] IN (SELECT [Id] FROM [Books] WHERE [AuthorId] = (SELECT [Id] FROM [Authors] WHERE [Name] = 'Alex Michaelides'));
+WHERE [BookId] IN (
+	SELECT [Id] 
+	FROM [Books] 
+	WHERE [AuthorId] IN (
+		SELECT [Id] 
+		FROM [Authors] 
+		WHERE [Name] = 'Alex Michaelides'
+	)
+)
 
 DELETE 
 FROM [Books]
-WHERE [AuthorId] = (SELECT [Id] FROM [Authors] WHERE [Name] = 'Alex Michaelides');
+WHERE [AuthorId] IN (
+	SELECT [Id] 
+	FROM [Authors] 
+	WHERE [Name] = 'Alex Michaelides'
+)
 
 DELETE 
 FROM [Authors]
-WHERE [Name] = 'Alex Michaelides';
+WHERE [Name] = 'Alex Michaelides'
