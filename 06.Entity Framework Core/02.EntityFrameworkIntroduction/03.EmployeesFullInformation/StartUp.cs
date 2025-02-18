a7c7d05e147f762f122c869ed1ce29632c368a1a
Alexandr0w <alexandrow2505@gmail.com>
2025-02-18 17:08:41 +0200

    Added exercises from Entity Framework Introduction

---
+++ 06.Entity Framework Core/02.EntityFrameworkIntroduction/03.EmployeesFullInformation/StartUp.cs
@@ -0,0 +1,41 @@
+﻿using SoftUni.Data;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            SoftUniContext dbContext = new SoftUniContext();
+            dbContext.Database.EnsureCreated();
+
+            string result = GetEmployeesFullInformation(dbContext);
+            Console.WriteLine(result);
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context.Employees
+                .OrderBy(e => e.EmployeeId)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.MiddleName,
+                    e.JobTitle,
+                    e.Salary
+                })
+                .ToArray();
+
+            foreach (var e in employees)
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary.ToString("F2")}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
