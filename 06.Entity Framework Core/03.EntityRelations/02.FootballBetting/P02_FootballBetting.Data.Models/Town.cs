8b2612cb137898280997d98e974895c70be39dca
Alexandr0w <alexandrow2505@gmail.com>
2025-02-21 17:51:16 +0200

    Added exercise from Entity Relations

---
+++ 06.Entity Framework Core/03.EntityRelations/02.FootballBetting/P02_FootballBetting.Data.Models/Town.cs
@@ -0,0 +1,23 @@
+﻿namespace P02_FootballBetting.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using static Common.EntityValidationConstants.Town;
+
+    public class Town
+    {
+        [Key]
+        public int TownId { get; set; }
+
+        [Required]
+        [MaxLength(TownNameMaxLength)]
+        public string Name { get; set; } = null!;
+
+        [ForeignKey(nameof(Country))]
+        public int CountryId { get; set; }
+        public virtual Country Country { get; set; } = null!;
+
+        public virtual ICollection<Team> Teams { get; set; } = new HashSet<Team>();
+        public virtual ICollection<Player> Players { get; set; } new HashSet<Player>();
+    }
+}
