a300635bb8b09f5a0bf355c43758101ee38cbaac
Alexandr0w <alexandrow2505@gmail.com>
2025-03-06 18:40:28 +0200

    Added exercise from Migrations

---
+++ 06.Entity Framework Core/06.Migrations/01.AcademicRecordsApp/Data/Models/Course.cs
@@ -0,0 +1,13 @@
+﻿namespace AcademicRecordsApp.Data.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public virtual ICollection<Exam> Exams { get; set; } = new HashSet<Exam>();
+
+        public virtual ICollection<StudentCourse> Students { get; set; } = new HashSet<StudentCourse>();
+    }
+}
---
+++ 06.Entity Framework Core/06.Migrations/01.AcademicRecordsApp/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace AcademicRecordsApp
+{
+    using Data;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using AcademicRecordsDbContext dbContext = new AcademicRecordsDbContext();
+
+            string[] students = dbContext.Students
+                .Select(s => s.FullName)
+                .ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, students));
+        }
+    }
+}
