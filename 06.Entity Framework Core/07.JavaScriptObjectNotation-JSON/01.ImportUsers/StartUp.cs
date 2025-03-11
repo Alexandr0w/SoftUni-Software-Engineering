c6d50613543245b5138e45172d4abea6e7c5ce97
Alexandr0w <alexandrow2505@gmail.com>
2025-03-11 15:14:53 +0200

    Added initial DB migration

--- 06.Entity Framework Core/07.JavaScriptObjectNotation-JSON/01.ImportUsers/StartUp.cs
+++ 06.Entity Framework Core/07.JavaScriptObjectNotation-JSON/01.ImportUsers/StartUp.cs
@@ -1,9 +1,16 @@
 ﻿namespace ProductShop
 {
+    using Data;
+    using Microsoft.EntityFrameworkCore;
+
     public class StartUp
     {
         public static void Main()
         {
+            using ProductShopContext dbContext = new ProductShopContext();
+
+            dbContext.Database.Migrate();
+            Console.WriteLine("Database migrated successfully!");
         }
     }
 }
