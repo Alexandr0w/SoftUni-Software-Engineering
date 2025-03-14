21bac3cb40eb3d2980b3d08d30e2c587605fe45f
Alexandr0w <alexandrow2505@gmail.com>
2025-03-15 01:40:49 +0200

    Migrate database for XML processing exercises

---
+++ 06.Entity Framework Core/08.ExtensibleMarkupLanguage-XML/09.ImportSuppliers/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using CarDealer.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using CarDealerContext dbContext = new CarDealerContext();
+            dbContext.Database.Migrate();
+
+            Console.WriteLine("Database migrated to the latest version successfully!");
+        }
+    }
+}
