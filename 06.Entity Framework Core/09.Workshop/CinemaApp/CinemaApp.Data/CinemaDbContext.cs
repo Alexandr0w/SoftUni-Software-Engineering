c11412b4f1f9eb906f11a71518c838bc4668f9bb
Alexandr0w <alexandrow2505@gmail.com>
2025-03-18 19:49:40 +0200

    DbContext renamed and namespaces are set as block scoped

---
+++ 06.Entity Framework Core/09.Workshop/CinemaApp/CinemaApp.Data/CinemaDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace CinemaApp.Data
+{
+    public class CinemaDbContext : IdentityDbContext
+    {
+        public CinemaDbContext(DbContextOptions<CinemaDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
