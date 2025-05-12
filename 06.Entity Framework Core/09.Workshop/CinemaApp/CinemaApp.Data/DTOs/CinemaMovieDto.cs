fdea1522c48dfc28e8515c967b8deb779b481689
Alexandr0w <alexandrow2505@gmail.com>
2025-05-12 03:10:58 +0300

    Uploaded DTOs and files

---
+++ 06.Entity Framework Core/09.Workshop/CinemaApp/CinemaApp.Data/DTOs/CinemaMovieDto.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using CinemaApp.Common.Constants;
+
+namespace CinemaApp.Data.Dtos
+{
+    public class CinemaMovieDto
+    {
+        [Required]
+        [MinLength(EntityConstants.Movie.TitleMinLength)]
+        [MaxLength(EntityConstants.Movie.TitleMaxLength)]
+        public string Movie { get; set; } = null!;
+
+        [Required]
+        [MinLength(EntityConstants.Cinema.NameMinLength)]
+        [MaxLength(EntityConstants.Cinema.NameMaxLength)]
+        public string Cinema { get; set; } = null!;
+
+        [Required]
+        [Range(EntityConstants.CinemaMovie.AvailableTicketsMinValue,
+            EntityConstants.CinemaMovie.AvailableTicketsMaxValue)]
+        public int AvailableTickets { get; set; }
+
+        [Required]
+        public bool IsDeleted { get; set; }
+
+        public string? Showtimes { get; set; }
+    }
+}
---
+++ 06.Entity Framework Core/09.Workshop/CinemaApp/CinemaApp.Data/DTOs/UserWatchlistDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+namespace CinemaApp.Data.Dtos
+{
+    using static Common.Constants.EntityConstants.ApplicationUser;
+
+    [XmlType("User")]
+    public class UserWatchlistDto
+    {
+        [Required]
+        [MinLength(UsernameMinLength)]
+        [MaxLength(UsernameMaxLength)]
+        [XmlAttribute(nameof(Username))]
+        public string Username { get; set; } = null!;
+
+        [Required]
+        [XmlArray(nameof(Movies))]
+        public UserWatchlistMovieDto[] Movies { get; set; } = null!;
+    }
+}
