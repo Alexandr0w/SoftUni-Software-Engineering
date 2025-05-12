c3d4cf5404cd26e89c4448a1f5ae1b47c62f925e
Alexandr0w <alexandrow2505@gmail.com>
2025-05-12 03:15:56 +0300

    Added Seeder classes

---
+++ 06.Entity Framework Core/09.Workshop/CinemaApp/CinemaApp.Data/Seeding/ApplicationDbContextSeeder.cs
@@ -0,0 +1,53 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using CinemaApp.Data.Seeding.Interfaces;
+
+namespace CinemaApp.Data.Seeding
+{
+    public class ApplicationDbContextSeeder : IDbSeeder
+    {
+        private readonly IServiceProvider serviceProvider;
+
+        private ICollection<IEntitySeeder> entitySeeders = null!;
+
+        public ApplicationDbContextSeeder(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+
+            this.InitializeDbSeeders();
+        }
+
+        public async Task SeedData()
+        {
+            foreach (IEntitySeeder entitySeeder in this.entitySeeders)
+            {
+                await entitySeeder.SeedEntityData();
+            }
+        }
+
+        private void InitializeDbSeeders()
+        {
+            this.entitySeeders = new List<IEntitySeeder>();
+            Type[] seedersToRequest = this.DiscoverDbSeeders(typeof(CinemaMovieSeeder).Assembly);
+            foreach (Type seederType in seedersToRequest)
+            {
+                object seederInstanceObj = this.serviceProvider
+                    .GetRequiredService(seederType);
+                if (seederInstanceObj is IEntitySeeder seederInstanceCast)
+                {
+                    this.entitySeeders.Add(seederInstanceCast);
+                }
+            }
+        }
+
+        private Type[] DiscoverDbSeeders(Assembly seedersAssembly)
+        {
+            Type[] seederTypes = seedersAssembly
+                .GetTypes()
+                .Where(t => t.GetInterfaces().Any(i => i.Name.Equals(nameof(IEntitySeeder))))
+                .ToArray();
+
+            return seederTypes;
+        }
+    }
+}
