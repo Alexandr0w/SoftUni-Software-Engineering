85627ca62ebfdf99dd035d34f20d0b335e43e0a5
Alexandr0w <alexandrow2505@gmail.com>
2025-11-30 20:02:11 +0200

    Uploaded exercises from Functions and Statements - Exercise

---
+++ 09.JS Front-End/03.FunctionsАndStatements/02.Exercise/01.SmallestOfThreeNumbers.js
@@ -0,0 +1,9 @@
+function findSmallest(a, b, c) {
+    const smallest = Math.min(a, b, c);
+    console.log(smallest);
+}
+
+findSmallest(2, 5, 3);
+findSmallest(600, 342, 123);
+findSmallest(25, 21, 4);
+findSmallest(2, 2, 2);
